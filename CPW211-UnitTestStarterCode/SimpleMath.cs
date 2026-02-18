a66c909cac758e252192d33f1f3d0a8ec6c9674c
Justin <justinburns2008@gmail.com>
2026-02-18 13:03:15 -0800

    Created Add

--- CPW211-UnitTestStarterCode/SimpleMath.cs
+++ CPW211-UnitTestStarterCode/SimpleMath.cs
@@ -15,7 +15,7 @@ public static class SimpleMath
 {
     public static double Add(double a, double b)
     {
-        throw new NotImplementedException();
+        return a + b;
     }
 
     public static double Subtract(double a, double b)
