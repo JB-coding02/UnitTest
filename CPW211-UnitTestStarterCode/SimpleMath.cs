c9b0b7fa5a3f68b19dc453d0ce1df6938aae3717
JB-coding02! <justinburns2008@gmail.com>
2026-02-18 16:43:06 -0800

    Created Multiply

--- CPW211-UnitTestStarterCode/SimpleMath.cs
+++ CPW211-UnitTestStarterCode/SimpleMath.cs
@@ -25,7 +25,7 @@ public static class SimpleMath
 
     public static double Multiply(double a, double b)
     {
-        throw new NotImplementedException();
+        return a * b;
     }
 
     public static double Divide(double a, double b)
